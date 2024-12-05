085882fcaad4de1740372920d785d6bb9dc19821
Alexey Karpov <kds539489@gmail.com>
2024-12-05 23:30:33 +0300

    Upd deleteItem

--- JS/deleteItem.js
+++ JS/deleteItem.js
@@ -1,17 +1,19 @@
 'use strict';
 
-const deleteButton = getDomElements().tableRowDeleteButton;  
+const deleteItem = () => {
+    const deleteButton = getDomElements().tableRowDeleteButton;  
 
-console.log(deleteButton);
+    deleteButton.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            const tableRow = button.closest('.table__row_good');
 
-deleteButton.forEach((button, index) => {
-    button.addEventListener('click', () => {
-        const tableRow = button.closest('.table__row_good');
+            if (tableRow) {
+                tableRow.remove();
+                delete obj[index];
+                console.log(obj);
+            }
+        })
+    });
+};
 
-        if (tableRow) {
-            tableRow.remove();
-            delete obj[index];
-            console.log(obj);
-        }
-    })
-});
+deleteItem();
